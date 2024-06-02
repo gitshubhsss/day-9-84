d27a3c2547c9b1238f85b0b1e08adf5a973e39d4
gitshubhsss <shubhamranjane16@gmail.com>
2024-06-02 22:04:44 +0530

    Array

---
+++ index.js
@@ -0,0 +1,17 @@
+let instaData={
+    cats:{
+        name:"catty",
+        followers:5,
+        following:10,
+        
+    },
+    dogs:{
+        name:"catty",
+        followers:5,
+        following:10,
+        
+    }
+}
+let username="cats";
+let data=instaData[username];
+console.log(data.name);
